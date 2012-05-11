8afb5cce0ad6d74ee7691e512d82040c636a3a5b
Tim Jones <tim@timjones.tw>
2012-05-11 22:18:07 +0800

    About as basic as you can get, but it *is* a ray-traced sphere

--- src/Aether.Studio/Modules/DemoSceneViewer/Scenes/SimpleScene.cs
+++ src/Aether.Studio/Modules/DemoSceneViewer/Scenes/SimpleScene.cs
@@ -1,7 +1,8 @@
 ﻿using Aether.Cameras;
 using Aether.Films;
 using Aether.Sampling;
-using Aether.Shapes;
+using Nexus;
+using Sphere = Aether.Shapes.Sphere;
 
 namespace Aether.Studio.Modules.DemoSceneViewer.Scenes
 {
@@ -10,10 +11,16 @@ namespace Aether.Studio.Modules.DemoSceneViewer.Scenes
 		public override Scene CreateScene(Film film)
 		{
 			var camera = new OrthographicCamera(film,
-			                                    1.0f, 1000.0f, Nexus.Vector3D.Forward, Nexus.Vector3D.Up,
-			                                    new Nexus.Point3D(0, 0, 25), film.XRes);
+				1.0f, 100.0f, Nexus.Vector3D.Forward, Nexus.Vector3D.Up,
+				new Nexus.Point3D(0, 0, 10), 20);
+			//var camera = new PerspectiveCamera(film,
+			//    1.0f, 100.0f, Nexus.Vector3D.Forward, Nexus.Vector3D.Up,
+			//    new Nexus.Point3D(0, 0, 10),
+			//    MathUtility.PI_OVER_4);
 
-			var sampler = new RegularSampler(new Nexus.IntPoint2D(0, 0), new Nexus.IntPoint2D(film.XRes, film.YRes));
+			var sampler = new RegularSampler(
+				new Nexus.IntPoint2D(0, 0),
+				new Nexus.IntPoint2D(film.XRes, film.YRes));
 
 			var shape = new Sphere(new Nexus.Point3D(0, 0, 0), 5);
 
